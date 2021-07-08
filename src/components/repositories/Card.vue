e385962718fbcd7b93c0930be758661bd703229d
NonOrganic <nonorganiccreature@gmail.com>
2021-07-08 19:31:04 +0400

    cards

--- src/components/repositories/Card.vue
+++ src/components/repositories/Card.vue
@@ -1,7 +1,23 @@
 <template>
     <div class="card">
         <div class="card-inner">
-
+            <div class="card-info">
+                <div class="card-info__item title">
+                    Name: {{ preparedCardInfo.name }}
+                </div>
+                <div class="card-info__item">
+                    <a :href="preparedCardInfo.html_url">Repository url</a>
+                </div>
+                <div class="card-info__item">
+                    Descriprion: {{ preparedCardInfo.description }}
+                </div>
+                <div class="card-info__item">
+                    Stars: {{ preparedCardInfo.stargazers_count }}
+                </div>
+                <div class="card-info__item">
+                    Forks: {{ preparedCardInfo.forks_count }}
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -16,17 +32,42 @@ export default {
             default: () => ({}),
         },
     },
-    computed: {},
-    created() {
-        
-    }
+    computed: {
+        preparedCardInfo() {
+            return {
+                ...this.cardInfo,
+                description: this.cardInfo.description
+                    ? this.cardInfo.description
+                    : "Empty",
+            };
+        },
+    },
 };
 </script>
 <style lang="scss">
 .card {
+    background: #888;
+    border: 1px solid #fff;
+    border-radius: 8px;
+    width: 300px;
     &-inner {
         display: flex;
         flex-flow: column nowrap;
+        padding: 10px;
     }
+    &-info {
+        display: flex;
+        flex-flow: column nowrap;
+        &__item {
+            &:first-child ~ * {
+                margin-top: 10px;
+                margin-left: 10px;
+            }
+            &.title {
+                font-weight: 700;
+            }
+        }
+    }
+
 }
 </style>
--- src/components/repositories/List.vue
+++ src/components/repositories/List.vue
@@ -1,28 +1,38 @@
 <template>
     <div class="list">
         <div class="list-inner">
-            <card v-for="repository in repositories" :key="repository.id"/>
+            <card
+                v-for="repository in repositories"
+                :key="repository.id"
+                :cardInfo="repository"
+            />
         </div>
     </div>
 </template>
 <script>
-import { mapGetters } from 'vuex'
-import Card from '@/components/repositories/Card'
+import { mapGetters } from "vuex";
+import Card from "@/components/repositories/Card";
 export default {
-    name: 'RepositoresList',
+    name: "RepositoresList",
     components: { Card },
     computed: {
         ...mapGetters({
-            repositories: 'repositories/getRepositories'
-        })
-    }
-}
+            repositories: "repositories/getRepositories",
+        }),
+    },
+};
 </script>
 <style lang="scss">
 .list {
+    width: 100%;
     &-inner {
         display: flex;
         flex-flow: row wrap;
+        padding: 30px;
+        justify-content: center;
+        & > *:not(:first-child) {
+            margin-left: 30px;
+        }
     }
 }
 </style>
